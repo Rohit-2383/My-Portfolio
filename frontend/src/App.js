7893b136d2d732e6bff7993dfdf34eb9c0a16003
Rohit-2383 <rohit.rai.2383@gmail.com>
2023-06-11 00:34:28 +0530

    assets setup and code for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+
 import React from 'react';
+import { About,Footer,Header,Skills,Testimonials,Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
   return (
-    <h1>fljfss</h1>
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonials/>
+      <Footer/>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+
+import React ,{useState} from 'react';
+import {HiMenuAlt4,HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion';
+import {images} from '../../constants';
+import './Navbar.scss';
+
+const Navbar = () => {
+    const [toggle, settoggle] = useState(false);
+  return (
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+            <img src={images.logo} alt='logo'/>
+        </div>
+        <ul className='app__navbar-links'>
+            {['home','about','work','skills','contact'].map((item)=>(
+                <li key={`link-${item}`} className='app__flex p-start'>
+                    <div></div>
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+
+            ))}
+        </ul>
+        <div className='app__navbar-menu'>
+                <HiMenuAlt4 onClick={()=>settoggle(true)}/>
+                {
+                    toggle && (
+                        <motion.div
+                            whileInView={{x:[300,0]}}
+                            transition={{duration:0.85,ease:'easeOut'}}
+                        >
+                            <HiX onClick={()=>settoggle(false)}/>
+                            <ul>
+                             {['home','about','work','skills','contact'].map((item)=>(
+                            <li key={item}>
+                                <a href={`#${item}`}  onClick={()=>settoggle(false)}>{item}</a>
+                            </li>
+                        ))}
+                            </ul>
+                        </motion.div>
+                    )
+                }
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar;
